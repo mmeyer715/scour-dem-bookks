499ffd014404631573b868d1dcd303f8c5838263
Maria Meyer <mbean1216@icloud.com>
2022-06-29 14:36:15 -0500

    adding saveBook and deleteBook resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -38,10 +38,31 @@ const resolvers = {
 
             const token = signToken(user);
             return {token, user}
-        }
+        },
 
         //TODO: Save Book
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: {savedBooks: args.newBook}},
+                    { new: true },
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You must be logged in!');
+        },
         //TODO: Delete Book
+        deleteBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: args.bookId} },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+        }
     }
 }
 
