8a93c5930f610c8da2b6a8ac8a552b1a597b0497
Maria Meyer <mbean1216@icloud.com>
2022-06-29 15:08:55 -0500

    editing typeDefs and adding SavedBookInput

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,24 +40,25 @@ const resolvers = {
             return {token, user}
         },
 
-        //TODO: Save Book
-        saveBook: async (parent, args, context) => {
-            if (context.user) {
-                const updatedUser = await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $push: {savedBooks: args.newBook}},
-                    { new: true },
-                );
-                return updatedUser;
-            }
-            throw new AuthenticationError('You must be logged in!');
+        //TODO: Save Book/ accepts a book author's array, description, title, bookId, image, and link as parameters
+        // returns a user type, look into creating whats know as an input type to handle all of these parameters
+        saveBook: async (parent, { bookData }, context) => {
+           if (context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: bookData }},
+                { new: true }
+            );
+            return updatedUser;
+           }
+           throw new AuthenticationError('You must be logged in!');
         },
         //TODO: Delete Book
-        deleteBook: async (parent, args, context) => {
+        deleteBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: args.bookId} },
+                    { $pull: { savedBooks: { bookId: bookId }} },
                     { new: true }
                 );
                 return updatedUser;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,23 +3,45 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql `
   type User {
     _id: ID
-    username: String!
-    email: String!
-    password: String!
-    savedBooks: [Book]!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
   }
   
   type Book {
-    _id: ID
-    authors: [{String}]
-    description: String!
     bookId: String!
+    authors: [String]
+    description: String
+    title: String
     image: String
-    title: String!
+    link: String
+  }
+
+  input SavedBookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
   }
 
   type Auth {
     token: ID!
     user: User
   }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: SavedBookInput): User
+    deleteBook(bookId: ID!): User
+  }
 `;
+
+module.exports = typeDefs;
