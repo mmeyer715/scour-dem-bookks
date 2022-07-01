451e7bd38ca12ca68bfc6e8b809e56f511e9fd21
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-07-01 11:46:02 -0500

    Resolve Front-End rendering issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
