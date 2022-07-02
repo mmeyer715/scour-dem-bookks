46d12a57405070b02ebea4154c9f2a20055bc255
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-07-01 19:10:33 -0500

    Messing with localSStorage

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -26,11 +26,10 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // Converted code to use Apoolo instead of routers
+  // Converted code to use Apollo instead of routers
   const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+  // set up useEffect hook to save `savedBookIds` list to localStorage on component amount
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
@@ -85,7 +84,6 @@ const SearchBooks = () => {
       const { data } = await saveBook({
         variables: { bookData: { ...bookToSave } },
       });
-      console.log(savedBookIds);
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
